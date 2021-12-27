4c28b751ca0bca0e81670b09395ed8dc692f2199
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-27 19:02:59 +0000

    setup server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Chat Backend");
+const fastify = require("fastify")({ logger: true });
+
+fastify.get("/", () => {
+  return "chat backend";
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(4040);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
