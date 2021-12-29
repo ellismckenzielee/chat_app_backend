8baacfa860bb7ea6a6dc9efa36e13cf14de08c9d
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-29 13:59:17 +0000

    create dbConnector plugin

---
+++ db/db-connector.js
@@ -0,0 +1,19 @@
+const fastifyPlugin = require("fastify-plugin");
+const dotenv = require("dotenv");
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({
+    path: "test.env",
+  });
+} else {
+  dotenv.config({
+    path: "prod.env",
+  });
+}
+async function dbConnector(fastify, options) {
+  console.log(process.env.URL);
+  fastify.register(require("fastify-mongodb"), {
+    url: process.env.URL,
+  });
+}
+
+module.exports = fastifyPlugin(dbConnector);
