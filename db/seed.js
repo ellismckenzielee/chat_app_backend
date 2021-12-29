cf9846eac426817bc22b6e063b8277369978398f
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-29 17:38:06 +0000

    add chat insert functionality to seed function

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,18 @@
-const { mongoose, User } = require("../db/connection");
+const { mongoose, User, Chat } = require("../db/connection");
 
-function seedDb(users) {
-  return User.collection.drop().then(() => {
-    return User.insertMany(users);
-  });
+function seedDb({ users, chats }) {
+  return User.collection
+    .drop()
+    .then(() => {
+      return User.insertMany(users);
+    })
+    .then(() => {
+      return Chat.collection.drop();
+    })
+    .then(() => {
+      console.log(chats);
+      return Chat.insertMany(chats);
+    })
+    .then(console.log);
 }
 module.exports = seedDb;
