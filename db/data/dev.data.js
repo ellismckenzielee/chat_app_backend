4fba97f0beaf95f251454f6e0a944c6591fb98d5
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-29 13:58:25 +0000

    add dev and test user data files

---
+++ db/data/dev.data.js
@@ -0,0 +1,8 @@
+const users = [
+  { name: "Ellis", username: "ellislee@gmail.com" },
+  { name: "Zoe", username: "zoeharries@gmail.com" },
+  { name: "Harry", username: "harrywaterworth@gmail.com" },
+  { name: "Corey", username: "coreylee@gmail.com" },
+];
+
+module.exports = users;
---
+++ db/data/test.data.js
@@ -0,0 +1,8 @@
+const users = [
+  { name: "Ellis", username: "ellislee" },
+  { name: "Zoe", username: "zoeharries" },
+  { name: "Harry", username: "harrywaterworth" },
+  { name: "Corey", username: "coreylee" },
+];
+
+module.exports = users;
