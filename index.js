5644f3527d1d4ad1c33343b2ef6705746a6dce4e
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-29 14:00:42 +0000

    fixes issue with fastify mongodb

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const fastify = require("fastify");
-const { mongoose, User } = require("./db/connection");
-
+const dbConnector = require("./db/db-connector");
+const routes = require("./routes/routes");
 function build(opts = {}) {
   const app = fastify(opts);
-  app.register(require("fastify-mongodb"), {
-    forceClose: true,
-    url: process.env.URL,
-  });
-  app.get("/", (req, res) => {
-    console.log("in / route");
-    res.code(200).send({ hello: "world" });
-  });
-  app.get("/users", (req, res) => {
-    console.log("in users route");
-    User.find()
-      .then((res) => {
-        return res;
-      })
-      .then((users) => {
-        res.code(200).send({ users });
-      });
-  });
-
-  app.get("/chats", (req, res) => {
-    return "chats page";
-  });
-
-  app.get("*", (req, res) => {
-    res.code(404).send({ msg: "Invalid URL" });
-  });
+  app.register(dbConnector);
+  app.register(routes);
   return app;
 }
 
