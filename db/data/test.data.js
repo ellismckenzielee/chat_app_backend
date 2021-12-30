800ba0fe6a4a490d360401fc6fb6bc41cf9430b9
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-30 12:44:01 +0000

    add message data

--- db/data/test.data.js
+++ db/data/test.data.js
@@ -5,6 +5,16 @@ const users = [
   { name: "Corey", username: "coreylee" },
 ];
 
-const chats = [{ users: ["ellislee", "zoeharries"] }, { users: ["ellislee", "harrywaterworth"] }];
+const chats = [
+  { users: ["ellislee", "zoeharries"], chatId: 1 },
+  { users: ["ellislee", "harrywaterworth"], chatId: 2 },
+];
 
-module.exports = { users, chats };
+const messages = [
+  { chatId: 1, sender: "ellislee", message: "Hello from Ellis!", sent: new Date() },
+  { chatId: 1, sender: "zoeharries", message: "Hello Ellis!!!", sent: new Date() },
+  { chatId: 1, sender: "ellislee", message: "Good morning Harry.", sent: new Date() },
+  { chatId: 2, sender: "ellislee", message: "Welcome to the warzone!", sent: new Date() },
+  { chatId: 2, sender: "harrywaterworth", message: "Morning.", sent: new Date() },
+];
+module.exports = { users, chats, messages };
