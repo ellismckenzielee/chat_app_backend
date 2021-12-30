8a37160c96db3cec741ce03bbf3c2e9d4edb1a03
Ellis Lee <ellismckenzielee@gmail.com>
2021-12-30 13:37:43 +0000

    add socket-io plugin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/user.routes");
 function build(opts = {}) {
   const app = fastify(opts);
   app.register(dbConnector);
+  app.register(require("fastify-socket.io"));
   app.register(userRoutes);
   app.register(chatRoutes);
   app.register(routes);
