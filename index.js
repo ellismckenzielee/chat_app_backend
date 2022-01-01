4983e20e3337a6b80f2ede906b67e41294a69575
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-01 20:36:33 +0000

    add socket and cors plugins

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const routes = require("./routes/general.routes");
 const userRoutes = require("./routes/user.routes");
 function build(opts = {}) {
   const app = fastify(opts);
+
+  app.register(require("fastify-cors"), {
+    origin: "http://localhost:5500",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  });
+  app.register(require("fastify-socket.io"), {
+    origin: "http://localhost:5500",
+  });
   app.register(dbConnector);
-  app.register(require("fastify-socket.io"));
+
   app.register(userRoutes);
   app.register(chatRoutes);
   app.register(routes);
