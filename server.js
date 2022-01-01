b5d480eee454af5ade6df9db62779fb8211bc0a0
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-01 20:37:48 +0000

    alter server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require("./index")({
   },
 });
 
-server.listen(3000, (err, address) => {
+server.listen(4500, (err, address) => {
   console.log(server.printRoutes());
   if (err) {
     server.log.error(err);
