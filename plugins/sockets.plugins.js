20403eddb4dced21b94dc043a60b23fee096d334
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-12 18:22:49 +0000

    add socket functionality to emit and detect new chat

--- plugins/sockets.plugins.js
+++ plugins/sockets.plugins.js
@@ -1,7 +1,13 @@
 const fastifyPlugin = require("fastify-plugin");
+const { Socket } = require("socket.io");
 const { postMessage } = require("../models/chats.models");
 const socketPlugin = async (fastify, options) => {
+  const clientPool = {};
   fastify.io.on("connection", (socket) => {
+    socket.on("register", (username) => {
+      console.log("register");
+      clientPool[username] = socket.id;
+    });
     socket.on("join", (chatId) => {
       socket.join(chatId);
       socket.emit("joined", chatId);
@@ -12,8 +18,25 @@ const socketPlugin = async (fastify, options) => {
       console.log("moveroom");
       socket.leave(socket.room);
     });
-    socket.on("disconnect", () => {
-      console.log("goodbye");
+
+    socket.on("newchat", (username) => {
+      console.log("new chat");
+      if (Object.keys(clientPool).includes(username)) {
+        fastify.io.to(clientPool[username], chatId);
+      }
+    });
+
+    socket.on("leave", () => {
+      console.log(clientPool);
+      const clientKeys = Object.keys(clientPool);
+      let socketKey;
+      for (let i = 0; i < clientKeys.length; i++) {
+        if (clientPool[clientKeys[i]] === socket.id) {
+          socketKey = clientKeys[i];
+        }
+      }
+      delete clientPool[socketKey];
+      console.log(clientPool);
     });
     socket.on("message", async (message) => {
       console.log("message", message);
@@ -22,7 +45,10 @@ const socketPlugin = async (fastify, options) => {
       message._id = response.insertedId.toString();
       console.log(message);
       fastify.io.to(socket.room).emit("message", message);
-      console.log("emitted");
+    });
+    socket.on("chat", (chat) => {
+      console.log("new chat", chat);
+      fastify.io.emit("chat", "chatId");
     });
   });
 };
