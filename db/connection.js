74afcfc17ad53c739367da67790155a175c08cde
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-17 10:53:41 +0000

    update User schema to include img url

--- db/connection.js
+++ db/connection.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "test") {
 
 const url = process.env.URL || process.env.DBURL;
 mongoose.connect(url);
-const User = mongoose.model("User", { name: String, username: String });
+const User = mongoose.model("User", { name: String, username: String, img: String });
 const Chat = mongoose.model("Chat", { users: Array, chatId: Number });
 const Message = mongoose.model("Message", { chatId: String, sender: "string", message: "string", sent: Date });
 module.exports = { mongoose, User, Chat, Message };
